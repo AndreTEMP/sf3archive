19ad6066db3a456a16609c053bf1afda45422319
andrehochiwork@gmail.com <wsd35@txstate.edu>
2024-10-26 02:45:25 -0500

    matches DB created

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -8,6 +8,8 @@ import {
   serial,
   timestamp,
   varchar,
+  integer,
+  boolean,
 } from "drizzle-orm/pg-core";
 
 /**
@@ -33,4 +35,42 @@ export const posts = createTable(
   (example) => ({
     nameIndex: index("name_idx").on(example.name),
   })
+);
+
+export const matchs = createTable(
+  "matches",
+  {
+    id: serial("id").primaryKey(),
+    video: varchar("video", { length: 256 }),
+    timeStampedHours: integer("time_stamped_hours"),
+    timeStampedMinutes: integer("time_stamped_minutes"),
+    timeStampedSeconds: integer("time_stamped_seconds"),
+    startingSeconds: integer("starting_seconds"),
+    hyperlinkSeconds: varchar("hyperlink_seconds", { length: 256 }),
+    playerOneName: varchar("player_one_name", { length: 256 }),
+    playerOneChar: varchar("player_one_char", { length: 256 }),
+    playerOneSA: varchar("player_one_s_a", { length: 256 }),
+    playerOneWinLoss: boolean("player_one_win_loss", { length: 256 }),
+    playerTwoName: varchar("player_two_name", { length: 256 }),
+    playerTwoChar: varchar("player_two_char", { length: 256 }),
+    playerTwoSA: varchar("player_two_s_a", { length: 256 }),
+    playerTwoWinLoss: boolean("player_two_win_loss", { length: 256 }),
+    playerOnePerfect: boolean("player_one_perfect", { length: 256 }),
+    playerTwoPerfect: integer("player_two_perfect", { length: 256 }),
+    mirror: integer("mirror", { length: 256 }),
+    event: varchar("event", { length: 256 }),
+    type: varchar("type", { length: 256 }),
+    date: integer("date"),
+    location: varchar("location", { length: 256 }),
+    breakdown: varchar("breakdown", { length: 256 }),
+    version: varchar("version", { length: 256 }),
+    ytDescription: varchar("yt_description", { length: 256 }),
+
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date()
+    ),
+  },
 );
