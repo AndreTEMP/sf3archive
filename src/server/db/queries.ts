d7477a1e019ff9235a34733c9c8f08c9aa83a84b
wsd35@txstate.edu <wsd35@txstate.edu>
2025-01-30 16:26:14 -0600

    added a bunch of ignores

--- src/server/db/queries.ts
+++
@@ -1,9 +0,0 @@
-import "server-only";
-import { db } from "./db";
-import { matches } from "./db/schema";
-
-export async function uploadMatches(matchesToBeUploaded: []) {
-
-  await db.insert(matches).values(matchesToBeUploaded);
-
-}
